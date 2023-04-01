2ef5727ce7ff82722ee03658f9c2b8088f149763
Ignacio López <83306936+iglopezp@users.noreply.github.com>
2023-04-01 19:03:10 -0300

    teste api de cotação

--- HTTP_requests/conexao_apy.py
+++ HTTP_requests/conexao_apy.py
@@ -1,5 +1,8 @@
 import json #Biblioteca para manipular objetos Json
 import requests #Biblioteca para fazer requisições HTTP
 
+moeda = 'USD-BRL'
+url = f'https://economia.awesomeapi.com.br/json/last/{moeda}'
 
-
+cotacao = requests.get(url)
+print(cotacao.content)
